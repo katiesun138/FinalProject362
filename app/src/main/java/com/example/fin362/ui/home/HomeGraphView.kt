b523a19264a9d7d862bcecd7d12e617751eefd48
Alice <aom1@sfu.ca>
2023-12-02 12:33:38 -0800

    Graph improvements
    Export button works, visuals of bar chart improved

--- app/src/main/java/com/example/fin362/ui/home/HomeGraphView.kt
+++ app/src/main/java/com/example/fin362/ui/home/HomeGraphView.kt
@@ -1,6 +1,8 @@
 package com.example.fin362.ui.home
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,8 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.core.view.drawToBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.fin362.R
@@ -21,7 +25,11 @@ import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.utils.ColorTemplate
+import java.io.File
+import java.io.FileOutputStream
+import java.util.Calendar
 
 class HomeGraphView(jobList: List<Job>) : Fragment() {
     private val internalJobList = jobList
@@ -53,9 +61,10 @@ class HomeGraphView(jobList: List<Job>) : Fragment() {
     private fun createBarChart(view: View) {
         val chartFrame = view.findViewById<FrameLayout>(R.id.history_graph_chart)
         val chart = com.github.mikephil.charting.charts.BarChart(requireContext())
-        val entries: ArrayList<BarEntry> = ArrayList()
-        val dataSet = BarDataSet(entries, "Application History")
         val legend = chart.legend
+        val dataSets: ArrayList<IBarDataSet> = arrayListOf()
+        var numBars = 0
+        var graphMax = 0
 
         val entryCounter = arrayOf(0, 0, 0, 0, 0)
 
@@ -70,26 +79,51 @@ class HomeGraphView(jobList: List<Job>) : Fragment() {
         }
 
         for (i in entryCounter.indices) {
-            val entry = BarEntry(i.toFloat(), entryCounter[i].toFloat())
             if (entryCounter[i] > 0) {
-                entries.add(entry)
+                val entry = BarEntry(numBars.toFloat(), entryCounter[i].toFloat())
+                numBars++
+                val entryHolder: ArrayList<BarEntry> = ArrayList()
+
+                val dataSet = when(i){
+                    0 -> BarDataSet(entryHolder, "Applied")
+                    1 -> BarDataSet(entryHolder, "Interviewing")
+                    2 -> BarDataSet(entryHolder, "Rejected")
+                    3 -> BarDataSet(entryHolder, "Offer")
+                    else -> BarDataSet(entryHolder, "Unknown")
+                }
+
+                entryHolder.add(entry)
+                dataSet.color = ColorTemplate.PASTEL_COLORS[numBars]
+                dataSets.add(dataSet)
+
+                if(entryCounter[i] > graphMax) { graphMax = entryCounter[i] }
             }
         }
 
         val xAxis = chart.xAxis
         xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.axisMinimum = -0.5f
+        xAxis.axisMaximum = numBars.toFloat() - 0.5f
+        xAxis.labelCount = numBars
+        xAxis.textSize = 0f
+
+        val yAxis = chart.axisLeft
+        yAxis.axisMinimum = 0f
+        yAxis.axisMaximum = graphMax.toFloat()
 
         legend.orientation = Legend.LegendOrientation.HORIZONTAL
         legend.setDrawInside(false)
 
-        dataSet.values = entries
-        for (i in ColorTemplate.PASTEL_COLORS) {
-            dataSet.colors.add(i)
-        }
+        val barData = BarData(dataSets)
+        barData.barWidth = 0.9f
 
-        val barData = BarData(dataSet)
+        Log.d("entry",dataSets.toString())
+        Log.d("entry",barData.toString())
 
         chart.data = barData
+        chart.description.isEnabled = false
+
+        Log.d("entry",chart.data.toString())
         chartFrame.addView(chart)
     }
 
@@ -131,8 +165,29 @@ class HomeGraphView(jobList: List<Job>) : Fragment() {
         chart.setEntryLabelTextSize(15f)
         chart.holeRadius = 50f
         chart.setUsePercentValues(false)
+        chart.description.isEnabled = false
 
         chartFrame.addView(chart)
+    }
+
+    private fun exportImage(view: View){
+        val chartFrame = view.findViewById<FrameLayout>(R.id.history_graph_chart)
+        val image = chartFrame.drawToBitmap()
+
+        val filePathAsString = requireActivity().getExternalFilesDir(null).toString() + "/" + getString(R.string.app_name)
+        val filePath = File(filePathAsString)
+        if(!filePath.exists()){ filePath.mkdir() }
+
+        val imageName = "JobChart_" + Calendar.getInstance().timeInMillis.toString() + ".png"
+        val file = File(filePath, imageName)
+        val outputStream = FileOutputStream(file)
+
+        image.compress(Bitmap.CompressFormat.PNG, 85, outputStream)
+
+        outputStream.flush()
+        outputStream.close()
+
+        Toast.makeText(context, "Chart exported as $imageName!", Toast.LENGTH_LONG).show()
     }
 
     override fun onCreateView(
@@ -155,6 +210,8 @@ class HomeGraphView(jobList: List<Job>) : Fragment() {
                 1 -> createBarChart(view)
             }
         }
+
+        view.findViewById<Button>(R.id.history_export_button).setOnClickListener{ exportImage(view) }
 
         return view
     }
